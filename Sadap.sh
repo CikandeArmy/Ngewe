085ebda211298259ba4054840b731e5449aba99d
CikandeArmy <50131505+CikandeArmy@users.noreply.github.com>
2019-07-17 19:01:31 +0700

    Add files via upload

---
+++ Sadap.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/bash
+#Tools SadapWhatsApp
+#Sadap WhatsApp
+#By Dark4127
+#Jangan DiRecord Tod!!!
+#Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $blue "<|            Tools SadapWhatsAPP      |>"
+echo  $blue "<|                                     |>"
+echo  $blue "<| Name       : Dark4127               |>"
+echo  $blue "<| MyTeam     : Solo Player            |>"
+echo  $blue "<| Contact    : 089613**********       |>"
+echo  $blue "<| YouTube    : Hack Team              |>"
+echo  $blue "<|======================================|>"
+sleep 1
+echo  $red "============== $red MENU $green ================="
+echo $blue "================================================="
+echo $blue "|| $green 1. Sadap WhatsApp No Indosat   $blue ||"
+echo $blue "|| $green 2. Sadap WhatsApp No Telkomsel $blue ||"
+echo $blue "|| $green 3. Sadap WhatsApp No XL / Axis $blue ||"
+echo $blue "|| $green 4. Sadap WhatsApp No 3(Three)  $blue ||"
+echo $blue "|| $green 0. Exit Sadap WhatsApp         $blue ||"
+echo $blue "================================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@Dark4127=•>" Dark4127;
+if [ $Dark4127 = 1 ] || [ $Dark4127 = 01 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@Dark4127=•>" Dark4127;
+sleep 2
+echo $red "[+]Tunggu Sedang Membuka Database Whatsapp "
+sleep 3
+echo $blue "Kena Tipu wkwkwk"
+sleep 4
+echo $green "Kasian Amat Lu Wibu"
+sleep 2
+echo $cyan "Lagian Ngapain Lu Nyadap Wa"
+sleep 8
+echo $purple "Lu Kan Jomblo"
+sleep 3
+echo $blue "Bagusan Lu Hode "
+sleep 2
+echo $red "Jual Burid Lu"
+sleep 5
+echo $green "wkwkwkwk"
+sleep 6
+echo $cyan "sorry"
+sleep 7
+echo $purple "cmn becanda sayang"
+sleep 3
+echo $blue "100%"
+sleep 2
+echo $green "Prosessing Done"
+sleep 4
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Verifikasi"fi
+fi
+
+if [ $Dark4127 = 2 ] || [ $Dark4127 = 02 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@Dark4127=•>" Dark4127;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 5
+echo $blue "kena tipu wkwkwkwk"
+sleep 3
+echo $green "kasian amat hidup lu cuk"
+sleep 8
+echo $cyan "lu kan jomblo ngapain nyadap wa"
+sleep 3
+echo $purple "bagusan lu hode"
+sleep 6
+echo $blue "lu jual buriq lu"
+sleep 5
+echo $red "Dark4127 Soloplayer"
+sleep 4
+echo $green "jgn baper kima"
+sleep 6
+echo $cyan "hanya becanda"
+sleep 8
+echo $purple "ummmch cadi sayang deh"
+sleep 6
+echo $blue "tapi bo'ong"
+sleep 9
+echo $green "Prosessing Done"
+sleep 3
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Verifikasi"
+fi
+if [ $Dark4127 = 3 ] || [ $Dark4127 = 03 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────rootDark4127=•>" Dark4127;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 8
+echo $blue "Berhasil Di Sadap"
+sleep 4
+echo $green "Tapi Bo'ong wkwkwkw"
+sleep 5
+echo $cyan "lu Ngapain Nyadap WA lu kan Jomblo"
+sleep 6
+echo $purple "Bagusan Lu Hode"
+sleep 4
+echo $blue "lu jual burid lu"
+sleep 2
+echo $red "Dark4127 SoloPlayer"
+sleep 3
+echo $green "Umcch Jadi Pengen Penggal Kepala lu"
+sleep 9
+echo $cyan "sorry tod cmn becanda"
+sleep 7
+echo $purple "jgn baper ya"
+sleep 4
+echo $blue "next time"
+sleep 9
+echo $green "Prosessing Done"
+sleep 9
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Nomer $Kode Verifikasi"
+fi
+
+if [ $Dark4127 = 4 ] || [ $Dark4127 = 04 ]
+then
+echo $blue"╭──[Silahkan Masukan Nomernya]>"
+read -p"╰───────root@Dark4127=•>" Dark4127;
+sleep 2
+echo $red "[+]Scaning...."
+sleep 8
+echo $blue "wkwkw kena Tipu"
+sleep 7
+echo $green "lu Kan jomblo ngapain nyadap wa pacar ngk ada"
+sleep 5
+echo $cyan "bagusan lu hode jual burid lu wkwkwk"
+sleep 4
+echo $purple "hacked by Dark Fb"
+sleep 5.5
+echo $blue "soloplayer"
+sleep 4
+echo $red "sorry ya sayang"
+sleep 5
+echo $green "cumn Becanda"
+sleep 6
+echo $cyan "80%"
+sleep 7
+echo $purple "90%"
+sleep 5
+echo $blue "100%"
+sleep 3
+echo $green "Prosessing Done"
+sleep 4
+echo  $blue "Nomer WhatsApp Berhasil Di Sadap $Nomer $Kode Verifikasi"
+fi
+
+if [ $MrTamfanX = 0 ] || [ $MrTamfanX = 00 ]
+then
+echo  $red "Bye Bye Sayang"
+echo  $blue "Jangan Lupa Col*"
+echo  $green "Dark4127 SoloPlayer"
+exit
+fi
